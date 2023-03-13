b94cc9a1f5f0bbcd8377caa1d413f710a13ff80f
artem.sedinin.2002@inbox.ru <artem.sedinin.2002@inbox.ru>
2023-03-13 23:25:45 +0600

    Добавьте файлы проекта.

---
+++ Gradient_Metod/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gradient_Metod
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+
+		}
+		private static void Metod_Gradient() { 
+			
+		}
+		private static double Func(double X1, double X2) {
+			return 1;
+		}
+	}
+}
