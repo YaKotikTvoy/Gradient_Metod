5f0d01e7ef3211b960148d6bd8ff4901dcc21fb0
artem.sedinin.2002@inbox.ru <artem.sedinin.2002@inbox.ru>
2023-03-16 15:39:42 +0600

    Gradient_method3 is valid

--- Gradient_Method2/Program.cs
+++ Gradient_Method2/Program.cs
@@ -15,7 +15,7 @@ namespace Gradient_Method2
 		private static double[] Gradient_Method(double x1, double x2, double alpha, double epsilome) {
 			double Xk1 = x1_next(x1, x2, ref alpha);
 			double Xk2 = x2_next(x1, x2, ref alpha);
-			for (int i = 0; Math.Abs(f(x1, x2) - f(x1 - df_dx2(x1, x2) * alpha, x2 - df_dx2(x1, x2) * alpha)) > epsilome; i++){
+			for (int i = 0; Math.Abs(f(x1, x2) - f(x1 - df_dx1(x1, x2) * alpha, x2 - df_dx2(x1, x2) * alpha)) > epsilome; i++){
 				Xk1 = x1_next(x1, x2, ref alpha);
 				Xk2 = x2_next(x1, x2, ref alpha);
 				x1 = Xk1;
@@ -27,13 +27,15 @@ namespace Gradient_Method2
 		private static double x1_next(double x1, double x2, ref double alpha) {
 			/*while (f(x1, x2) > f(x1 - df_dx2(x1, x2) * alpha, x2 - df_dx2(x1, x2) * alpha)) alpha *= 0.5;
 			return x1 - df_dx2(x1, x2) * alpha;*/
-			//double f_n = f(x1, x2);
-			//double fn = f(x1 - df_dx1(x1, x2) * alpha, x2 - df_dx2(x1, x2) * alpha);
+			double f_n = f(x1, x2);
+			double fn = f(x1 - df_dx1(x1, x2) * alpha, x2 - df_dx2(x1, x2) * alpha);
 			while (f(x1, x2) < f(x1 - df_dx1(x1, x2) * alpha, x2 - df_dx2(x1, x2) * alpha)) alpha *= 0.5;
-			return x1 - df_dx2(x1, x2) * alpha;
+			return x1 - df_dx1(x1, x2) * alpha;
 		}
 		private static double x2_next(double x1, double x2, ref double alpha) {
-			while (f(x1, x2) < f(x1 - df_dx1(x1, x2) * alpha, x2 - df_dx2(x1, x2) * alpha)) alpha *= 0.5;
+			double f_n = f(x1, x2);
+			double fn = f(x1 - df_dx1(x1, x2) * alpha, x2 - df_dx2(x1, x2) * alpha);
+			while (f(x1, x2) <= f(x1 - df_dx1(x1, x2) * alpha, x2 - df_dx2(x1, x2) * alpha)) alpha *= 0.5;
 			return x2 - df_dx2(x1, x2) * alpha;
 		}
 		private static double f(double x1, double x2) {
@@ -43,7 +45,7 @@ namespace Gradient_Method2
 			return 2 * x1 + 2 * Math.Pow(Math.E, Math.Pow(x1, 2) + Math.Pow(x2, 2)) + 4;
 		}
 		private static double df_dx2(double x1, double x2) {
-			return 2 * x2 * Math.Pow(Math.E, Math.Pow(x1, 2) + Math.Pow(x2, 2)) + 2 * x2 + 3;
+			return 2 * x2 * Math.Pow(Math.E, Math.Pow(x1, 2) + Math.Pow(x2, 2)) + 3;
 		}
 	}
 }
---
+++ Gradient_method3/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gradient_method3
+{
+	internal class Program
+	{
+		static void Main(string[] args){
+			double[] Xmin = Gradient_const_step(1, 1, 0.1, 1E-6);
+			Console.WriteLine($"x1 = {Xmin[0]}, x2 = {Xmin[1]} f({Xmin[0]},{Xmin[1]}) = {f(Xmin[0], Xmin[1])}");
+			Console.ReadKey();
+		}
+		private static double[] Gradient_const_step(double x1, double x2, double alpha, double epsilome) {
+			double Xk1 = x1_next(x1, x2, alpha);
+			double Xk2 = x2_next(x1, x2, alpha);
+			for (int i = 0; Math.Abs(f(Xk1, Xk2) - f(x1, x2)) > epsilome; i++ ) {
+				x1 = Xk1;
+				x2 = Xk2;
+				Xk1 = x1_next(x1, x2, alpha);
+				Xk2 = x2_next(x1, x2, alpha);
+				while (f(Xk1, Xk2) >= f(x1, x2)) {
+					alpha *= 0.5;
+					Xk1 = x1_next(x1, x2, alpha);
+					Xk2 = x2_next(x1, x2, alpha);
+
+				}
+				Console.WriteLine($"Итерация {i + 1}.X1n = {Xk1}, X2n = {Xk2}, f({Xk1}, {Xk2}) = {f(Xk1, Xk2)}, f(X1n-1, X2n-1) = {f(Xk1, Xk2)}.");
+			}
+			return new double []{ Xk1, Xk2};
+		}
+		private static double x1_next(double x1, double x2, double alpha) {
+			//while (f(x1 - df_x1(x1, x2) * alpha, x2 - df_x2(x1, x2) * alpha) > f(x1,x2) ) alpha *= 0.5;
+			return x1 - df_x1(x1, x2) * alpha;	
+		}
+		private static double x2_next(double x1, double x2, double alpha) {
+			//while (f(x1 - df_x1(x1, x2) * alpha, x2 - df_x2(x1, x2) * alpha) > f(x1, x2)) alpha *= 0.5;
+			return x2 - df_x2(x1, x2) * alpha;
+		}
+		private static double f(double x1, double x2) { 
+			//return Math.Pow(x1, 2) + Math.Pow(Math.E, Math.Pow(x1, 2) + Math.Pow(x2, 2)) + 4 * x1 + 3 * x2;
+			return Math.Pow(x1,2) + Math.Pow(x2,2);
+		}
+		private static double df_x1(double x1, double x2) {
+			//return 2 * x1 + 2 * x1 * Math.Pow(Math.E, Math.Pow(x1, 2) + Math.Pow(x2, 2)) + 4;
+			return 2 * x1;
+		}
+		private static double df_x2(double x1, double x2) {
+			//return 2 * x2 * Math.Pow(Math.E, Math.Pow(x1, 2) + Math.Pow(x2, 2)) + 3;
+			return 2 * x2;
+		}
+	}
+}
